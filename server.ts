b36dfb5767df665e8c230684b71836cb404ae7d6
faridneki <pro.benazzouz@gmail.com>
2026-08-08 13:47:10 +0100

    modification 171717171717

--- server.ts
+++ server.ts
@@ -30,6 +30,17 @@ const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json({ limit: '10mb' }));
 
+// CORS & Headers middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Seed PostgreSQL database if empty
 seedCloudSQLIfEmpty();
 
